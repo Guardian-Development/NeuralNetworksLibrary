23a530bc93ce57678ea5fd8cfa94c31fabfc6940
Joe Honour <joehonour@hotmail.co.uk>
2017-06-21 19:20:06 +0100

    Adding some basic unit tests around sigmoid activation function

---
+++ NeuralNetworks.Tests.Support/Assertors/DoubleAssertionHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace NeuralNetworks.Tests.Support.Assertors
+{
+    public sealed class DoubleAssertionHelpers
+    {
+        public static int DoubleAssertionPrecision { get; set; } = 15;
+
+        public static void AssertEqualWithinPrecision(double expected, double actual)
+        {
+            Assert.Equal(expected, actual, DoubleAssertionPrecision);
+        }
+    }
+}
--- NeuralNetworks.Tests.UnitTests/ActivationFunctionTests/SigmoidActivationFunctionTests.cs
+++ NeuralNetworks.Tests.UnitTests/ActivationFunctionTests/SigmoidActivationFunctionTests.cs
@@ -1,16 +1,27 @@
 ﻿using NeuralNetworks.Library.Components.Activation.Functions;
+using NeuralNetworks.Tests.Support.Assertors;
 using Xunit;
 
 namespace NeuralNetworks.Tests.UnitTests.ActivationFunctionTests
 {
     public sealed class SigmoidActivationFunctionTests
     {
-        [Fact]
-        public void ActivateProducesCorrectResultPositiveValue()
+        [Theory]
+        [InlineData(0.3775, 0.593269992107187)]
+        public void ActivateProducesCorrectResultPositiveValue(double activationValue, double expectedResult)
         {
             var activationFunction = SigmoidActivationFunction.Create();
-            var activationResult = activationFunction.Activate(0.3775); 
-            Assert.Equal(0.593269992, activationResult);
+            var activationResult = activationFunction.Activate(activationValue);
+            DoubleAssertionHelpers.AssertEqualWithinPrecision(expectedResult, activationResult);
+        }
+
+        [Theory]
+        [InlineData(-0.1, 0.47502081252106)]
+        public void ActivateProducesCorrectResultNegativeValue(double activationValue, double expectedResult)
+        {
+            var activationFunction = SigmoidActivationFunction.Create();
+            var activationResult = activationFunction.Activate(activationValue);
+            DoubleAssertionHelpers.AssertEqualWithinPrecision(expectedResult, activationResult);
         }
     }
 }
