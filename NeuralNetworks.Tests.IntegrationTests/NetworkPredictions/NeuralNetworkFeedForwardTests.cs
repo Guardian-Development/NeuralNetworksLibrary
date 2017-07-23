18fcef047928f8958ec8ed2d9a40d9abc9bfbff5
Guardian-Development <joehonour@hotmail.co.uk>
2017-07-23 19:04:31 +0100

    adding structure for remaining tests

--- NeuralNetworks.Tests.IntegrationTests/NetworkPredictions/NeuralNetworkFeedForwardTests.cs
+++ NeuralNetworks.Tests.IntegrationTests/NetworkPredictions/NeuralNetworkFeedForwardTests.cs
@@ -267,16 +267,147 @@ namespace NeuralNetworks.Tests.IntegrationTests.NetworkPredictions
                     expectedOutput: new [] { 0.62054 });
         }
 
-        [Fact]
+        [Fact(Skip="Not completed manual number calculations")]
         public void CanFeedForwardCorrectlyMultipleInputNeuronsMultipleHiddenLayersMultipleOutputNeurons()
         {
-
+            FeedForwardTester()
+                .NeuralNetworkEnvironment(TestContext, PredictableGenerator)
+                .TargetNeuralNetwork(nn => nn
+                    .InputLayer(l => l
+                        .Neurons(
+                            n => n.Id(1).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(2).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(3).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(4).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(5).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(6).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .OutputLayer(l => l
+                        .Neurons(
+                            n => n.Id(7).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(8).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(9).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .Synapses(
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 3, weight: 0.67),
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 4, weight: 0.72),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 3, weight: 0.145),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 4, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 3, outputNeuronId: 5, weight: 0.9),
+                        s => s.SynapseBetween(inputNeuronId: 4, outputNeuronId: 5, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 5, outputNeuronId: 6, weight: 0.11),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 7, weight: 0.781),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 8, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 9, weight: 0.7101)))
+                .InputAndExpectOutput(
+                    inputs: new [] { 0.12345, 0.9812 },
+                    expectedOutput: new [] { 0.0, 0.0, 0.0 });
         }
 
-        [Fact]
-        public void CanFeedForwardCorrectlyInComplexMultiLayeredNeuralNetwork()
+        [Fact(Skip="Not completed manual number calculations")]
+        public void CanFeedForwardCorrectlyInComplexMultiLayerNeuralNetwork()
         {
-
+            FeedForwardTester()
+                .NeuralNetworkEnvironment(TestContext, PredictableGenerator)
+                .TargetNeuralNetwork(nn => nn
+                    .InputLayer(l => l
+                        .Neurons(
+                            n => n.Id(1).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(2).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(3).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(4).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(5).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(6).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(7).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(8).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(9).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(10).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(11).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(12).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .HiddenLayer(l => l
+                        .Neurons(
+                            n => n.Id(13).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(14).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(15).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(16).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .OutputLayer(l => l
+                        .Neurons(
+                            n => n.Id(17).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(18).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(19).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(20).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .Synapses(
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 6, weight: 0.67),
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 7, weight: 0.72),
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 8, weight: 0.145),
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 9, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 6, weight: 0.9),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 7, weight: 0.9),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 8, weight: 0.9),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 9, weight: 0.9),
+                        s => s.SynapseBetween(inputNeuronId: 3, outputNeuronId: 6, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 3, outputNeuronId: 7, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 3, outputNeuronId: 8, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 3, outputNeuronId: 9, weight: 0.134),
+                        s => s.SynapseBetween(inputNeuronId: 4, outputNeuronId: 6, weight: 0.11),
+                        s => s.SynapseBetween(inputNeuronId: 4, outputNeuronId: 7, weight: 0.11),
+                        s => s.SynapseBetween(inputNeuronId: 4, outputNeuronId: 8, weight: 0.11),
+                        s => s.SynapseBetween(inputNeuronId: 4, outputNeuronId: 9, weight: 0.11),
+                        s => s.SynapseBetween(inputNeuronId: 5, outputNeuronId: 6, weight: 0.781),
+                        s => s.SynapseBetween(inputNeuronId: 5, outputNeuronId: 7, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 5, outputNeuronId: 8, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 5, outputNeuronId: 9, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 10, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 11, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 6, outputNeuronId: 12, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 7, outputNeuronId: 10, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 7, outputNeuronId: 11, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 7, outputNeuronId: 12, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 8, outputNeuronId: 10, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 8, outputNeuronId: 11, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 8, outputNeuronId: 12, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 9, outputNeuronId: 10, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 9, outputNeuronId: 11, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 9, outputNeuronId: 12, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 10, outputNeuronId: 13, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 10, outputNeuronId: 14, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 10, outputNeuronId: 15, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 10, outputNeuronId: 16, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 11, outputNeuronId: 13, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 11, outputNeuronId: 14, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 11, outputNeuronId: 15, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 11, outputNeuronId: 16, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 12, outputNeuronId: 13, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 12, outputNeuronId: 14, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 12, outputNeuronId: 15, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 12, outputNeuronId: 16, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 13, outputNeuronId: 17, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 13, outputNeuronId: 18, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 13, outputNeuronId: 19, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 13, outputNeuronId: 20, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 14, outputNeuronId: 17, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 14, outputNeuronId: 18, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 14, outputNeuronId: 19, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 14, outputNeuronId: 20, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 15, outputNeuronId: 17, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 15, outputNeuronId: 18, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 15, outputNeuronId: 19, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 15, outputNeuronId: 20, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 16, outputNeuronId: 17, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 16, outputNeuronId: 18, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 16, outputNeuronId: 19, weight: 0.7101),
+                        s => s.SynapseBetween(inputNeuronId: 16, outputNeuronId: 20, weight: 0.7101)))
+                .InputAndExpectOutput(
+                    inputs: new [] { 0.0, 0.0, 0.0, 0.0, 0.0 },
+                    expectedOutput: new [] { 0.0, 0.0, 0.0 });
         }
     }
 }
