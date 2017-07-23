fb1d5a99b91a5c01c577d3d7fa9f936666c1ff73
Guardian-Development <joehonour@hotmail.co.uk>
2017-07-23 10:13:49 +0100

    adding first feed forward tests

--- NeuralNetworks.Tests.IntegrationTests/NetworkPredictions/NeuralNetworkFeedForwardTests.cs
+++ NeuralNetworks.Tests.IntegrationTests/NetworkPredictions/NeuralNetworkFeedForwardTests.cs
@@ -10,6 +10,7 @@ namespace NeuralNetworks.Tests.IntegrationTests.NetworkPredictions
     {
         public Func<NeuralNetworkPredictionsTester> FeedForwardTester 
             => NeuralNetworkPredictionsTester.Create;
+            
         private NeuralNetworkContext TestContext => 
             new NeuralNetworkContext(
                 errorRateDecimalPlaces: 5, 
@@ -29,7 +30,27 @@ namespace NeuralNetworks.Tests.IntegrationTests.NetworkPredictions
                     .Synapses(s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 2, weight: 0.15)))
                 .InputAndExpectOutput(
                     inputs: new [] { 0.1 },
-                    expectedOutput: new [] { 0.5 }); 
+                    expectedOutput: new [] { 0.50375 });
+        }
+
+        [Fact]
+        public void CanFeedForwardOutputCorrectlyForMultipleInputNeuronsNoHiddenLayerSingleOutputNeuron()
+        {
+             FeedForwardTester()
+                .NeuralNetworkEnvironment(TestContext, PredictableGenerator)
+                .TargetNeuralNetwork(nn => nn
+                    .InputLayer(l => l
+                        .Neurons(
+                            n => n.Id(1).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid),
+                            n => n.Id(2).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .OutputLayer(l => l
+                        .Neurons(n => n.Id(3).ErrorRate(0).Output(0).Activation(ActivationType.Sigmoid)))
+                    .Synapses(
+                        s => s.SynapseBetween(inputNeuronId: 1, outputNeuronId: 3, weight: 0.87),
+                        s => s.SynapseBetween(inputNeuronId: 2, outputNeuronId: 3, weight: 0.11)))
+                .InputAndExpectOutput(
+                    inputs: new [] { 0.4, 0.56 },
+                    expectedOutput: new [] { 0.60099 });
         }
     }
 }
