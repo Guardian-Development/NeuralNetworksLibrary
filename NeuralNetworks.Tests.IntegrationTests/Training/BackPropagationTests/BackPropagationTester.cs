caec3bb35a4b98d1bee8625a32b1580b07c9b206
Guardian-Development <joehonour@hotmail.co.uk>
2017-08-19 18:21:37 +0100

    missed tester extension

--- NeuralNetworks.Tests.IntegrationTests/Training/BackPropagationTests/BackPropagationTester.cs
+++ NeuralNetworks.Tests.IntegrationTests/Training/BackPropagationTests/BackPropagationTester.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using NeuralNetworks.Library;
 using NeuralNetworks.Library.NetworkInitialisation;
 using NeuralNetworks.Library.Training.BackPropagation;
+using NeuralNetworks.Tests.Support;
 using NeuralNetworks.Tests.Support.Builders;
 
 namespace NeuralNetworks.Tests.IntegrationTests.Training.BackPropagationTests
 {
-    public sealed class BackPropagationTester
+    public sealed class BackPropagationTester : NeuralNetworkTester<BackPropagationTester>
     {
         private readonly double learningRate;
         private readonly double momentum;
-        private NeuralNetworkContext context; 
-        private IProvideRandomNumberGeneration randomGenerator; 
-        private NeuralNetwork targetNeuralNetwork; 
 
         private readonly List<TrainingEpochTester<BackPropagation>> trainingStates 
             = new List<TrainingEpochTester<BackPropagation>>(); 
@@ -22,23 +20,6 @@ namespace NeuralNetworks.Tests.IntegrationTests.Training.BackPropagationTests
         {
             this.momentum = momentum;
             this.learningRate = learningRate;
-        }
-
-        public BackPropagationTester NeuralNetworkEnvironment(
-            NeuralNetworkContext context, 
-            IProvideRandomNumberGeneration randomGenerator)
-        {
-            this.context = context; 
-            this.randomGenerator = randomGenerator;
-            return this; 
-        }
-
-        public BackPropagationTester TargetNeuralNetwork(Action<ExplicitNeuralNetworkBuilder> action)
-        {
-            var neuralNetworkBuilder = ExplicitNeuralNetworkBuilder.CreateForTest(context, randomGenerator); 
-            action.Invoke(neuralNetworkBuilder); 
-            targetNeuralNetwork = neuralNetworkBuilder.Build(); 
-            return this; 
         }
 
         public BackPropagationTester QueueTrainingEpoch(Action<TrainingEpochTester<BackPropagation>> action)
