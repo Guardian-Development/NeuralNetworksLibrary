614bf133d3df51a3d0ba98856c15e71feb3d9522
Guardian-Development <joehonour@hotmail.co.uk>
2017-08-21 20:52:47 +0100

    * adding initial perf tests

---
+++ NeuralNetworks.Tests.PerformanceTests/FeedForwardPerformanceTestContainer.cs
@@ -0,0 +1,38 @@
+using BenchmarkDotNet.Attributes;
+using NeuralNetworks.Library;
+using NeuralNetworks.Library.Components.Activation;
+using NeuralNetworks.Library.Extensions;
+
+namespace NeuralNetworks.Tests.PerformanceTests
+{
+    public class FeedForwardPerformanceComparisonContainer
+    {
+        private readonly NeuralNetwork neuralNetworkUnderTest;
+
+        public FeedForwardPerformanceComparisonContainer()
+        {
+            neuralNetworkUnderTest = 
+                NeuralNetwork
+                    .For(NeuralNetworkContext.MaximumPrecision)
+                    .WithInputLayer(neuronCount: 5, activationType: ActivationType.Sigmoid)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.Sigmoid)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.TanH)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.TanH)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.Sigmoid)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.TanH)
+                    .WithHiddenLayer(neuronCount: 100, activationType: ActivationType.Sigmoid)
+                    .WithOutputLayer(neuronCount: 2, activationType: ActivationType.Sigmoid)
+                    .Build();
+        }
+
+        private double[] InputValues => new [] { 0.98, 0.23, 0.44, 0.44, 0.12 }; 
+
+        [Benchmark]
+        public double[] PredictSingleThreaded() => 
+            neuralNetworkUnderTest.PredictionFor(InputValues, ParallelOptionsExtensions.SingleThreadedOptions()); 
+
+        [Benchmark]
+        public double[] PredictMultiThreaded() => 
+            neuralNetworkUnderTest.PredictionFor(InputValues, ParallelOptionsExtensions.UnrestrictedMultiThreadedOptions()); 
+    }
+}
---
+++ NeuralNetworks.Tests.PerformanceTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using NeuralNetworks.Library;
+using NeuralNetworks.Library.Components.Activation;
+using NeuralNetworks.Library.Extensions;
+
+namespace NeuralNetworks.Tests.PerformanceTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<FeedForwardPerformanceComparisonContainer>();
+        }
+    }
+}
