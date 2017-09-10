0443e5c5ce5a1cc33448d3453c1eb8bbfdc6b30c
Joe <joehonour@hotmail.co.uk>
2017-09-10 17:11:30 +0100

    * adding some more reporting

--- ExampleProjects/FraudDetection/NeuralNetworks.Examples.FraudDetection.Web/Models/PredictionReport.cs
+++ ExampleProjects/FraudDetection/NeuralNetworks.Examples.FraudDetection.Web/Models/PredictionReport.cs
@@ -1,8 +1,22 @@
+using System;
+
 namespace NeuralNetworks.Examples.FraudDetection.Web.Models
 {
     public class PredictionReport 
     {
         public int NumberOfCorrectPredictions { get; set; }
         public int NumberOfIncorrectPredictions { get; set; }
+
+        public string CorrectPercentage 
+        {
+            get {
+                var total = NumberOfCorrectPredictions + NumberOfIncorrectPredictions;
+                if(total == 0) return $"{0:00.00}%";  
+
+                var percentage = (Convert.ToDouble(NumberOfCorrectPredictions) / Convert.ToDouble(total)) * 100; 
+                return $"{((percentage)):00.00}%";
+            }
+        }
+           
     }
 }
